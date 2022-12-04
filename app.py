dabb0ad419b9f2463a58417c3d8ba5193ce101de
cjuberaUNCC <cjubera@uncc.edu>
2022-12-04 15:05:38 -0500

    create main page

---
+++ app.py
@@ -0,0 +1,28 @@
+import altair as alt
+import streamlit as st
+import pandas as pd
+
+
+st.title("Crime in the United Sates")
+
+tab1, tab2, tab3 = st.tabs(["Main", "Crime", "State"])
+
+with tab1:
+   st.header("What this tool does:")
+   st.markdown("This tool uses crime data collected from a variety of communinties across the US combined with census data to highlight " + 
+   "potential links in community attributes and types of crime.")
+   st.header("What this tool is for:")
+   st.markdown("Our web app can be used to inform  " + 
+   "potential links in community attributes and types of crime.")
+   st.header("Who can use this tool:")
+   st.header("Additional Info:")
+   st.markdown("Made by: Prachi Chandanshive, Abel Villanueva Perez, Nikhil Mandge, and Charlie Jubera \n" + 
+   "Data sourced from: https://www.kaggle.com/datasets/michaelbryantds/crimedata/code")
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
