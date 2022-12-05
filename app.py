9f88096799af5899c3a3adb64c7ecf495c6128aa
cjuberaUNCC <cjubera@uncc.edu>
2022-12-05 11:01:37 -0500

    sidebar for crime page

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ tab1, tab2, tab3, tab4 = st.tabs(["Main", "Crime","Predict Crime", "State"])
 @st.cache
 def load_crime_data():
    crime_df = pd.read_csv("crimedata.csv")
-   crime_corr = pd.read_csv("corr.csv")
-   
+   crime_corr = pd.read_csv("corr.csv", index_col=0)
+   crime_corr = crime_corr.reset_index()
    return crime_df, crime_corr
 
 with tab1:
@@ -29,22 +29,22 @@ with tab1:
    st.markdown("Made by: Prachi Chandanshive, Abel Villanueva Perez, Nikhil Mandge, and Charlie Jubera.")
    st.markdown("Data sourced from: https://www.kaggle.com/datasets/michaelbryantds/crimedata/code.")
 with tab2:
+   
    crime_df, crime_corr = load_crime_data()
    with st.sidebar:
-    crime_corr = crime_corr.reset_index(level=0)
     census_stats = ['population','householdsize','racepctblack','racePctWhite','racePctAsian','racePctHisp','agePct12t21','agePct12t29','agePct16t24','agePct65up','numbUrban','pctUrban','medIncome','pctWWage','pctWFarmSelf','pctWInvInc','pctWSocSec','pctWPubAsst','pctWRetire','medFamInc','perCapInc','whitePerCap','blackPerCap','indianPerCap','AsianPerCap','OtherPerCap','HispPerCap','NumUnderPov','PctPopUnderPov','PctLess9thGrade','PctNotHSGrad','PctBSorMore','PctUnemployed','PctEmploy','PctEmplManu','PctEmplProfServ','PctOccupManu','PctOccupMgmtProf','MalePctDivorce','MalePctNevMarr','FemalePctDiv','TotalPctDiv','PersPerFam','PctFam2Par','PctKids2Par','PctYoungKids2Par','PctTeen2Par','PctWorkMomYoungKids','PctWorkMom','NumKidsBornNeverMar','PctKidsBornNeverMar','NumImmig','PctImmigRecent','PctImmigRec5','PctImmigRec8','PctImmigRec10','PctRecentImmig','PctRecImmig5','PctRecImmig8','PctRecImmig10','PctSpeakEnglOnly','PctNotSpeakEnglWell','PctLargHouseFam','PctLargHouseOccup','PersPerOccupHous','PersPerOwnOccHous','PersPerRentOccHous','PctPersOwnOccup','PctPersDenseHous','PctHousLess3BR','MedNumBR','HousVacant','PctHousOccup','PctHousOwnOcc','PctVacantBoarded','PctVacMore6Mos','MedYrHousBuilt','PctHousNoPhone','PctWOFullPlumb','OwnOccLowQuart','OwnOccMedVal','OwnOccHiQuart','OwnOccQrange','RentLowQ','RentMedian','RentHighQ','RentQrange','MedRent','MedRentPctHousInc','MedOwnCostPctInc','MedOwnCostPctIncNoMtg','NumInShelters','NumStreet','PctForeignBorn','PctBornSameState','PctSameHouse85','PctSameCity85','PctSameState85']
     crimes = ['murders','murdPerPop','rapes','rapesPerPop','robberies','robbbPerPop','assaults','assaultPerPop','burglaries','burglPerPop','larcenies','larcPerPop','autoTheft','autoTheftPerPop','arsons','arsonsPerPop','ViolentCrimesPerPop','nonViolPerPop']
     
     crime_type = st.selectbox("What crime type would you like to get more info on?", crimes)
     census_selection = st.selectbox('What census statistic would you like to visualize your selected crimes with?', census_stats)
-    crime_selected_corr = crime_corr[['index', 'murders']].sort_values(by='murders', ascending=False)
+    crime_selected_corr = crime_corr[['index', crime_type]].sort_values(by=crime_type, ascending=False)
     filtered_corr = crime_selected_corr[crime_selected_corr['index'].isin(census_stats)]['index']
     st.markdown("### Census Statistics with the Highest Correlation to your Selected Crime Type \n " + 
-    '1.' + filtered_corr.iloc[0] + "\n" ) 
-   #  '2.' + filtered_corr.iloc[] + "\n" + 
-   #  '3.' + filtered_corr.iloc[] + "\n" + 
-   #  '4.' + filtered_corr.iloc[] + "\n" + 
-   #  '5.' + filtered_corr.iloc[])
+    '1. ' + filtered_corr.iloc[0] + "\n" +
+    '2. ' + filtered_corr.iloc[1] + "\n" + 
+    '3. ' + filtered_corr.iloc[2] + "\n" + 
+    '4. ' + filtered_corr.iloc[3] + "\n" + 
+    '5. ' + filtered_corr.iloc[4])
 
 with tab3:
    st.header("An owl")
